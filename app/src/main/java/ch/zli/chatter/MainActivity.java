86a23c4fc79ecff3519f1ac9e22e9343cd4fcea7
TobjaF <74833361+TobjaF@users.noreply.github.com>
2021-11-22 11:43:42 +0100

    SignInActivity erstellt, intent für signinactivity in MainActivity implementiert

--- app/src/main/java/ch/zli/chatter/MainActivity.java
+++ app/src/main/java/ch/zli/chatter/MainActivity.java
@@ -1,9 +1,16 @@
 package ch.zli.chatter;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 import ch.zli.chatter.R;
 
@@ -12,6 +19,10 @@ import ch.zli.chatter.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private LinearLayoutManager manager;
+    private FirebaseDatabase db;
+    private FirebaseAuth auth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +30,14 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+
+        auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() == null){
+            startActivity(new Intent(this, SignInActivity.class));
+            finish();
+            return;
+        }
+
 
     }
 }
---
+++ app/src/main/java/ch/zli/chatter/SignInActivity.java
@@ -0,0 +1,85 @@
+package ch.zli.chatter;
+
+import static java.util.Collections.list;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import ch.zli.chatter.databinding.ActivitySignInBinding;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SignInActivity extends AppCompatActivity {
+
+    private ActivitySignInBinding binding;
+
+    // --->>> private ActivityResultLauncher<Intent> signIn = registerForActivityResult(FirebaseAuthUIActivityResultContract.class, this.onSignInResult);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+        binding = ActivitySignInBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        if(FirebaseAuth.getInstance().getCurrentUser() == null){
+            // Choose authentication providers
+            List<AuthUI.IdpConfig> providers = Arrays.asList(
+                    new AuthUI.IdpConfig.EmailBuilder().build(),
+                    new AuthUI.IdpConfig.GoogleBuilder().build());
+
+            Intent signInIntent = AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setAvailableProviders(providers)
+                    .build();
+
+            // --->>> signIn.launch(signInIntent);
+        } else {
+            goToMainActivity();
+        }
+    }
+
+    private void goToMainActivity() {
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+
+
+    private void onSignInResult(FirebaseAuthUIAuthenticationResult result) {
+        if (result.getResultCode() == RESULT_OK) {
+            Log.d("SignInActivity", "Sign in successful!");
+            goToMainActivity();
+        } else {
+            Toast.makeText(
+                    this,
+                    "There was an error signing in",
+                    Toast.LENGTH_LONG).show()
+
+            IdpResponse response = result.getIdpResponse();
+
+            if (response == null) {
+                Log.w("SignInActivity", "Sign in canceled");
+            } else {
+                Log.w("SignInActivity", "Sign in error", response.getError());
+            }
+        }
+    }
+}
