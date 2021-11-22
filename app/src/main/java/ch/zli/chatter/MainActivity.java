a72455946a03273a474792221e607790f681b73e
TobjaF <74833361+TobjaF@users.noreply.github.com>
2021-11-22 10:56:10 +0100

    viewbinding für Main Activity umgesetzt

--- app/src/main/java/ch/zli/chatter/MainActivity.java
+++ app/src/main/java/ch/zli/chatter/MainActivity.java
@@ -3,14 +3,22 @@ package ch.zli.chatter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import ch.zli.chatter.R;
 
+import ch.zli.chatter.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
     }
 }
